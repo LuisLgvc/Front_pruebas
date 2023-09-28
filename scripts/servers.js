472986f771da862154f22e0a918898b2f7909701
LuisLgvc <113144443+LuisLgvc@users.noreply.github.com>
2023-09-28 17:24:17 -0300

    H E L P 5.0

--- scripts/servers.js
+++ scripts/servers.js
@@ -6,6 +6,7 @@ function obtenerServidores() {
     console.log(id_usuario);
 
     const serverList = document.getElementById('server-list');
+    const channelsList = document.getElementById('channels-list');
     // Realizar una solicitud fetch para obtener el JSON de servidores
     const url = `http://127.0.0.1:5000/server/${id_usuario}`;
 
@@ -18,17 +19,38 @@ function obtenerServidores() {
             console.log(data);
 
             // Recorrer los datos y crear elementos <li> para cada servidor
+            // data.forEach(serverItem => {
+            //     // localStorage.setItem("server", JSON.stringify(serverItem));
+            //     const listItem = document.createElement('li');
+            //     listItem.textContent = serverItem[0]; // Acceder al primer elemento de la lista interna
+            //     serverList.appendChild(listItem);
+
+            //     listItem.addEventListener('click', function() {
+            //         const serverName = serverItem[0];
+            //         console.log(serverName);
+            //         // Ahora puedes utilizar el nombre del servidor como desees
+            //         obtenerCanales(serverName)
+            //     });
+            // });
             data.forEach(serverItem => {
-                // localStorage.setItem("server", JSON.stringify(serverItem));
                 const listItem = document.createElement('li');
                 listItem.textContent = serverItem[0]; // Acceder al primer elemento de la lista interna
                 serverList.appendChild(listItem);
-
+            
                 listItem.addEventListener('click', function() {
                     const serverName = serverItem[0];
                     console.log(serverName);
                     // Ahora puedes utilizar el nombre del servidor como desees
-                    obtenerCanales(serverName)
+                    obtenerCanales(serverName);
+            
+                    // Agregar un botón para crear canales en el servidor seleccionado
+                    const createChannelButton = document.createElement('button');
+                    createChannelButton.textContent = 'Crear Canal';
+                    createChannelButton.addEventListener('click', function() {
+                        // Llamar a una función para crear un canal en el servidor seleccionado
+                        crearCanal(serverName);
+                    });
+                    channelsList.appendChild(createChannelButton);
                 });
             });
         })
